8c0111c52b092b7301f80c8f9dbf37b0fcbe55ac
CoderGirl <angi.adema@gmail.com>
2023-05-17 14:03:19 -0600

    added code to search-pg.html and functionality in script.js. Updated style.css.

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+var searchEl = document.querySelector(".search");
+
+function handleSearchSubmit(event) {
+  event.preventDefault();
+
+  var searchInput = document.querySelector("#search");
+  var formatInput = document.querySelector("#format");
+
+  if (!searchInput) {
+    console.error("Please enter a search parameter.");
+    return;
+  }
+
+  var queryString = "./search/?q=" + searchInput + "&format=" + formatInput;
+
+  location.assign(queryString);
+}
+
+searchEl.addEventListener("submit", handleSearchSubmit);
